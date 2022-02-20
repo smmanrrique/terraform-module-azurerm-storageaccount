56f4bb4971cace2afb002721a05cec0b71700993
smmanrrique <smmanrrique@hiberus.com>
2022-02-20 16:22:48 +0100

    feat(output): fix primary key

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output "name" {
 
 output "primary_key" {
     description = "The primary access key for the storage account"
-    value = azurerm_storage_account.sa.primary_access_key
+    value = azurerm_storage_account.storage_account.primary_access_key
     sensitive   = true
 }
