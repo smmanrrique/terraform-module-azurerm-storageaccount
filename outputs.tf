e23622f83137be620b059a6ec888fd67439a2956
smmanrrique <smmanrrique@hiberus.com>
2022-02-20 14:15:20 +0100

    fix(output): added primary_key

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@
 output "name" {
   value = azurerm_storage_account.storage_account.name
 }
+
+output "primary_key" {
+    description = "The primary access key for the storage account"
+    value = azurerm_storage_account.sa.primary_access_key
+    sensitive   = true
+}
